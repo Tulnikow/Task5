4bfffde4e48b6ff981059ed221b127fe6c8299bb
Andrei <tulnikow@yandex.ru>
2022-09-15 16:40:31 +0300

    Commit append Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWoldbean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(helloWoldbean1.getMessage());
+        HelloWorld helloWoldbean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Сравнение для пары бинов типа HelloWold " + (helloWoldbean1 == helloWoldbean2));
+        System.out.println("Сравнение для пары бинов типа Cat " + (cat1 == cat2));
     }
 }
